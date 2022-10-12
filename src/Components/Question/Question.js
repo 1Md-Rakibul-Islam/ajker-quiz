fb37343ddad914b28b40a5c69354f6e4952da486
1Md-Rakibul-Islam <rakibul199tech@gmail.com>
2022-10-12 21:52:52 +0600

    quiz hidden answer option added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Col, Row } from 'react-bootstrap';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { ToastContainer, toast } from 'react-toastify';
+import { EyeIcon } from '@heroicons/react/24/solid';
 import 'react-toastify/dist/ReactToastify.css';
 import './Question.css'
 
@@ -12,12 +13,14 @@ const Quest = ({Ques}) => {
     const { id, correctAnswer, options, question} = Ques
     const mainQues = question.slice(3, question.length - 4)
 
-
+//secection answer question tost 
     const quizAnswer = (option) => {
+        const dynamicToast = correctAnswer === option ? toast.success : toast.error
 
-        toast((correctAnswer === option ? 'Good Job. Your answer is Correct.' : 'Sorry, Your answer is Wrong'), {
+        dynamicToast(
+            (correctAnswer === option ? <h4 className='w-4 h-5 p-1' >Good Job! <small className='d-block'>Right Answer</small></h4> : <h4 className='w-4 h-5 p-1' >Sorry, <small className='d-block'>Wrong Answer</small></h4>), {
             position: "top-center",
-            autoClose: 5000,
+            autoClose: 4000,
             hideProgressBar: false,
             closeOnClick: true,
             pauseOnHover: true,
@@ -27,13 +30,20 @@ const Quest = ({Ques}) => {
         });
     }
 
+
     return (
         <ListGroup className="my-5 border">
             <h4 className='mt-3'>{mainQues}</h4>
+
+            {/* hidden currect answer show toas */}
+            <EyeIcon onClick={() => toast('Currect Answer:' + correctAnswer)} className='hidden-ans-icon text-primary mx-auto' />
+
             <Row xxs={1} xs={1} sm={1} md={2} lg={2} xl={2} xxl={2} className='m-3'>
                 {
-                    options.map(option => <Col><ListGroup.Item key={id} onClick={() => quizAnswer(option)}>{option}</ListGroup.Item></Col>)
-                
+                    options.map(option => <Col>
+                        <ListGroup.Item key={id} onClick={() => quizAnswer(option)}>{option}</ListGroup.Item>
+                    </Col>)
+
                 }
                 <ToastContainer />
             </Row>
